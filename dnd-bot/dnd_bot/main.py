fa9a37cc0f8ead736a43861271fe618064eabc2b
Mixss <michalziemiec@wp.pl>
2022-11-21 22:19:50 +0100

    Added slash commands support

--- dnd-bot/dnd_bot/main.py
+++ dnd-bot/dnd_bot/main.py
@@ -1,22 +1,25 @@
 import discord
 from discord.ext import commands
+from discord import app_commands
 
 intents = discord.Intents().all()
-client = commands.Bot(command_prefix= '!', intents=intents)
+client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
 
 @client.event
 async def on_ready():
+    await tree.sync()
     print('Bot has successfully started.')
 
-@client.command()
-async def hello(ctx):
-    await ctx.send("Hello world!")
+@tree.command(name="hello", description="Responds with greeting")#, guild=discord.Object(id=913375693864308797))
+async def hello(interaction):
+    await interaction.response.send_message("Hello")
 
 def main():
     devpass_file = open("../devpass.cfg")
     devpass = devpass_file.read()
     devpass_file.close()
-    
+
     client.run(devpass)
 
 main()
