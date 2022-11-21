922829213e182e2b8f260712532f1d1b8f674534
Mixss <michalziemiec@wp.pl>
2022-11-21 21:15:00 +0100

    main.py: Added hello world command

--- dnd-bot/dnd_bot/main.py
+++ dnd-bot/dnd_bot/main.py
@@ -1,4 +1,18 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents().all()
+client = commands.Bot(command_prefix= '!', intents=intents)
+
+@client.event
+async def on_ready():
+    print('Bot has successfully started.')
+
+@client.command()
+async def hello(ctx):
+    await ctx.send("Hello world!")
+
 def main():
-    print("Hello")
+    client.run('MTA0NDMwNzg2NzI2OTg2MTQwOA.G-6xOG.6OOnxia9luYir_dugHSITRxeZcRbBcbPuFXQ1E')
 
 main()
