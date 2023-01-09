0c3f6a444490b7d4d8b2d1260743faa0aac937af
Jakub <jkbl01@o2.pl>
2023-01-09 22:43:17 +0100

    dnd-bot/dnd_bot/logic/lobby/handler_join.py: add join exceptions

--- dnd-bot/dnd_bot/logic/lobby/handler_join.py
+++ dnd-bot/dnd_bot/logic/lobby/handler_join.py
@@ -13,7 +13,18 @@ class HandlerJoin:
                 - error message is the fourth argument
         """
 
-        game = Multiverse.get_game(token)
+        try:
+            game = Multiverse.get_game(token)
+        except KeyError:
+            return False, [], f':no_entry: No game found using this token!'
+
+        if game.game_state != 'LOBBY':
+            return False, [], f':warning: This game has already started!'
+
+        for user in game.user_list:
+            if user['discord_id'] == user_id:
+                return False, [], f':no_entry: You have already joined this game.'
+
         game.add_player(user_id, user_dm_channel, username)
 
         users = game.user_list
