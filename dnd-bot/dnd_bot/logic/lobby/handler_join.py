e74113f3e655658913d557eb838765b2d57a114c
Jakub <jkbl01@o2.pl>
2023-01-09 22:10:59 +0100

    dnd-bot: added players readiness

--- dnd-bot/dnd_bot/logic/lobby/handler_join.py
+++ dnd-bot/dnd_bot/logic/lobby/handler_join.py
@@ -1,5 +1,3 @@
-from dnd_bot.database.database_connection import DatabaseConnection
-from dnd_bot.dc.utils.utils import get_user_name_by_id
 from dnd_bot.logic.prototype.multiverse import Multiverse
 
 
@@ -15,35 +13,13 @@ class HandlerJoin:
                 - error message is the fourth argument
         """
 
-        game_data = DatabaseConnection.find_game_by_token(token)
-        if game_data is None:
-            return False, [], f':no_entry: No game found using this token!'
+        game = Multiverse.get_game(token)
+        game.add_player(user_id, user_dm_channel, username)
 
-        if game_data['game_state'] != 'LOBBY':
-            return False, [], f':warning: This game has already started!'
-
-        users = game_data['players']
-
-        for user in users:
-            if user['discord_id'] == user_id:
-                return False, [], f':no_entry: You have already joined this game.'
-
-        # handle join user operation
-        DatabaseConnection.add_user(game_data['id_game'], user_id)
-
-        game_data = DatabaseConnection.find_game_by_token(token)
-        users = game_data['players']
+        users = game.user_list
         lobby_players = []
 
         for user in users:
-            username = await get_user_name_by_id(user['discord_id'])
-            if user['discord_id'] == game_data['id_host']:
-                lobby_players.append((username, False, True, user['discord_id']))
-            else:
-                lobby_players.append((username, False, False, user['discord_id']))
-
-        # add data to Multiverse
-        game = Multiverse.get_game(token)
-        game.add_player(user_id, user_dm_channel, username)
+            lobby_players.append((user.username, user.is_ready, user.is_host, user.discord_id))
 
         return True, lobby_players, ""
--- dnd-bot/dnd_bot/logic/lobby/handler_start.py
+++ dnd-bot/dnd_bot/logic/lobby/handler_start.py
@@ -1,4 +1,4 @@
-from dnd_bot.database.database_connection import DatabaseConnection
+from dnd_bot.logic.prototype.multiverse import Multiverse
 
 
 class HandlerStart:
@@ -9,18 +9,15 @@ class HandlerStart:
     @staticmethod
     async def start_game(token, user_id) -> (bool, list, str):
 
-        game_data = DatabaseConnection.find_game_by_token(token)
-        if game_data is None:
+        game = Multiverse.get_game(token)
+        if game is None:
             return False, [], f':no_entry: Game of provided token doesn\'t exist!'
 
-        if user_id != game_data['id_host']:
+        if user_id != game.id_host:
             return False, [], f':warning: Only the host can start the game!'
 
-        if game_data['game_state'] == 'LOBBY' or game_data['game_state'] == 'STARTING':
-            DatabaseConnection.start_game(game_data['id_game'])
-
-            users = [user['discord_id'] for user in game_data['players']]
-
+        if game.game_state == 'LOBBY' or game.game_state == 'STARTING':
+            users = [user.discord_id for user in game.user_list]
             return True, users, ''
         else:
-            return False, f':no_entry: The game has already started!'
+            return False, [], f':no_entry: The game has already started!'
