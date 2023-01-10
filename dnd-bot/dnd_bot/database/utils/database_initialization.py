366438842c51401e4fec6acd5ac9047f438e12f0
Jakub <s184469@student.pg.edu.pl>
2023-01-10 22:10:29 +0100

    add python table creation script

---
+++ dnd-bot/dnd_bot/database/utils/database_initialization.py
@@ -0,0 +1,28 @@
+from psycopg2 import connect
+from dnd_bot.database.database_connection import DatabaseConnection
+
+"""
+Python script to initialize tables
+
+    It is meant to be used when starting a fresh development environment
+    This assumes the tables don't exist
+"""
+
+db_address, db_name, db_user, db_password, db_port = DatabaseConnection.__connection_get_authentication__()
+
+print('DB table initialization: attempting connection to {db_name} database at {db_address}:{db_port}')
+
+connection = connect(database=db_name, user=db_user, password=db_password,
+                     host=db_address, port=db_port)
+cursor = connection.cursor()
+
+print('DB table initialization: successfully connected')
+
+queries = open("../../../../database/scripts/create_tables.sql", "r").read()
+cursor.execute(queries)
+
+print('Tables created.')
+
+connection.commit()
+cursor.close()
+connection.close()
