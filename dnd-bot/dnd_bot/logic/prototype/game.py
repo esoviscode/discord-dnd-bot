bd5cca089e5ea1f54a149475a89b72c08e58c1d5
Mixss <michalziemiec@wp.pl>
2023-01-10 19:38:41 +0100

    comment game class

--- dnd-bot/dnd_bot/logic/prototype/game.py
+++ dnd-bot/dnd_bot/logic/prototype/game.py
@@ -1,7 +1,7 @@
 from dnd_bot.logic.prototype.user import User
-import copy
 
 
+# class represents particular games and lobbies
 class Game:
 
     def __init__(self, token, id_host=None, id_campaign=None, game_state="LOBBY", user_list=None):
@@ -15,15 +15,28 @@ class Game:
         self.user_list = user_list
 
     def add_player(self, user_id, user_channel_id, username):
+        """adds player to the game
+        :param user_id: users discord id
+        :param user_channel_id: private discord channel id
+        :param username: username
+        :return: None
+        """
         self.user_list.append(User(self.token, user_id, user_channel_id, username))
 
     def add_host(self, user_id, user_channel_id, username):
+        """ adds player as the host to the game
+        :param user_id: users discord id
+        :param user_channel_id: private discord channel id
+        :param username: username
+        :return: None
+        """
         self.id_host = user_id
         user = User(self.token, user_id, user_channel_id, username)
         user.is_host = True
         self.user_list.append(user)
 
     def all_users_ready(self):
+        """checks if all users are ready in lobby"""
         for user in self.user_list:
             if not user.is_ready:
                 return False
