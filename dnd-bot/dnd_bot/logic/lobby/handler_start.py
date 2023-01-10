1cd77c8892b079721f94f731724ea626106e80c3
Mixss <michalziemiec@wp.pl>
2023-01-10 19:49:18 +0100

    comment handler_start

--- dnd-bot/dnd_bot/logic/lobby/handler_start.py
+++ dnd-bot/dnd_bot/logic/lobby/handler_start.py
@@ -2,13 +2,17 @@ from dnd_bot.database.database_connection import DatabaseConnection
 
 
 class HandlerStart:
-
+    """handles starting of the game when all players in lobby are ready"""
     def __init__(self):
         pass
 
     @staticmethod
     async def start_game(token, user_id) -> (bool, list, str):
-
+        """in lobby, starts a game; have an effect when used by the host of the lobby
+        :param token: game token
+        :param user_id: id of the user who run the command or the host that pressed the start button
+        :return:
+        """
         game_data = DatabaseConnection.find_game_by_token(token)
         if game_data is None:
             return False, [], f':no_entry: Game of provided token doesn\'t exist!'
