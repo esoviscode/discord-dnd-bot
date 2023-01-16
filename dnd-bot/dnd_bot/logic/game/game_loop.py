1e2fd2bf1a08e92b2256dcafbba510e1a54e435f
Mixss <michalziemiec@wp.pl>
2023-01-16 22:18:16 +0100

    game_loop.py: implement prepare_queue

--- dnd-bot/dnd_bot/logic/game/game_loop.py
+++ dnd-bot/dnd_bot/logic/game/game_loop.py
@@ -1,3 +1,5 @@
+from dnd_bot.logic.prototype.creature import Creature
+from dnd_bot.logic.prototype.entity import Entity
 from dnd_bot.logic.prototype.game import Game
 
 
@@ -5,6 +7,7 @@ class GameLoop:
     """This class manages all logic behind turns. The idea of usage is that when player invokes a command/action then
     the static method of this class is called.
     """
+
     @staticmethod
     def begin_turn(game_id):
         """does all the necessities to begin the turn"""
@@ -13,10 +16,25 @@ class GameLoop:
     @staticmethod
     def prepare_queue(game: Game):
         """puts all the creatures to the queue with order by initiative"""
-        pass
+        def entity_sorting_value(e):
+            if isinstance(e, Entity):
+                return -1
+            else:
+                return e.initiative
+
+        # sort entities to put creatures into the queue in right order
+        game.entities.sort(reverse=True, key=entity_sorting_value)
+
+        # make sure that the queue is empty
+        if not game.creatures_queue.empty():
+            game.creatures_queue.queue.clear()
+
+        # add creatures to the queue
+        for c in game.entities:
+            if isinstance(c, Creature):
+                game.creatures_queue.put(c)
 
     @staticmethod
     def get_game_object(game_id):
         """returns game object from given id"""
         return None
-
