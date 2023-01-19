a679217b95291c5764e0f323b2c9a3b44c92f270
Paweł Cichowski <cichowski.pawel1@gmail.com>
2023-01-19 12:35:45 +0100

    game: handler_game.py: document function

--- dnd-bot/dnd_bot/logic/game/handler_game.py
+++ dnd-bot/dnd_bot/logic/game/handler_game.py
@@ -7,6 +7,7 @@ from dnd_bot.logic.prototype.multiverse import Multiverse
 class HandlerGame:
     @staticmethod
     def handle_game(token):
+        """creates a game loop running in the background in a parallel thread"""
         game = Multiverse.get_game(token)
 
         # create a separate thread running the main game loop
