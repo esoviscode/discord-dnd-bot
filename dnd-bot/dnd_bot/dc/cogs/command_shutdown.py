3702ab3faa6e9c73925f12a727c15b3e4a5ec7b8
Mixss <michalziemiec@wp.pl>
2023-01-19 20:27:44 +0100

    fully implemented bot shutdown

--- dnd-bot/dnd_bot/dc/cogs/command_shutdown.py
+++ dnd-bot/dnd_bot/dc/cogs/command_shutdown.py
@@ -2,6 +2,8 @@ from nextcord.ext import commands
 from nextcord.ext.commands import Cog, Bot
 from nextcord import slash_command
 
+from dnd_bot.logic.prototype.multiverse import Multiverse
+
 
 class ShutdownCommand(Cog):
 
@@ -11,8 +13,18 @@ class ShutdownCommand(Cog):
     @slash_command(name="shutdown", description="Shutdowns bot")
     async def shutdown(self, interaction):
         caller_id = interaction.user.id
-        if caller_id == 349553403229110274 or caller_id == 211188033968406530 or caller_id == 444538116569825290 or caller_id == 602785249025589259:
+        if caller_id == 349553403229110274 \
+                or caller_id == 211188033968406530 \
+                or caller_id == 444538116569825290 \
+                or caller_id == 602785249025589259:
             await interaction.response.send_message("Shutting down... :coffin:")
+
+            # stop all running game threads
+            for game in Multiverse.games.values():
+                game.game_state = "INACTIVE"
+                game.get_active_player().active = False
+                game.game_loop_thread.join()
+
             await self.bot.close()
         else:
             await interaction.response.send_message("You have no permission to shutdown me :man_gesturing_no:!")
