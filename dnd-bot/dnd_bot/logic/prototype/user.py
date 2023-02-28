0efdfb0ad397274870990b1eada479275343e2af
kapselccc <s184848@student.pg.edu.pl>
2023-02-28 13:13:01 +0100

    prototype/user: added type definition

--- dnd-bot/dnd_bot/logic/prototype/user.py
+++ dnd-bot/dnd_bot/logic/prototype/user.py
@@ -1,7 +1,7 @@
 class User:
     """defines discord user"""
 
-    def __init__(self, id_game, discord_id, channel_id, username, color):
+    def __init__(self, id_game: int = 0, discord_id: int = 0, channel_id: int = 0, username: str = "", color: str = ""):
         self.id_game = id_game
         self.discord_id = discord_id
         self.channel_id = channel_id
