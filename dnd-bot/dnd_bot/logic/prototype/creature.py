24f201821cb7faf4831dfa0ec3194175db9f509e
kapselccc <s184848@student.pg.edu.pl>
2023-02-28 13:22:55 +0100

    prototype/creature.py: added type definition

--- dnd-bot/dnd_bot/logic/prototype/creature.py
+++ dnd-bot/dnd_bot/logic/prototype/creature.py
@@ -5,9 +5,10 @@ from dnd_bot.logic.prototype.equipment import Equipment
 class Creature(Entity):
     """represents a creature"""
 
-    def __init__(self, x=0, y=0, sprite=None, name='Creature', hp=0, strength=0, dexterity=0,
-                 intelligence=0, charisma=0, perception=0, initiative=0, action_points=0,
-                 level=0, drop_equipment: Equipment = None, drop_money=0, items=None, game_token=''):
+    def __init__(self, x=0, y=0, sprite=None, name: str = 'Creature', hp: int = 0, strength: int = 0,
+                 dexterity: int = 0, intelligence: int = 0, charisma: int = 0, perception: int = 0, initiative: int = 0,
+                 action_points: int = 0, level: int = 0, drop_equipment: Equipment = None, drop_money: int = 0,
+                 items=None, game_token: str = ''):
         super().__init__(x=x, y=y, sprite=sprite, name=name, fragile=True, game_token=game_token)
         if items is None:
             items = []
