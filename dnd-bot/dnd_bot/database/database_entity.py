663c77e62f923412ae4e201fa9273b4f0ee46b67
kapselccc <s184848@student.pg.edu.pl>
2023-03-03 19:20:53 +0100

    added database_entity

---
+++ dnd-bot/dnd_bot/database/database_entity.py
@@ -0,0 +1,10 @@
+class DatabaseEntity:
+
+    def add_entity(self) -> int | None:
+        pass
+
+    def update_entity(self, x: int = 0, y: int = 0) -> None:
+        pass
+
+    def get_entity(self, id_entity: int) -> dict | None:
+        pass
