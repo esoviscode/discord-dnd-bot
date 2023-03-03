ca411bc28b58de3d09fcccc6daa92fac43b9e3c6
kapselccc <s184848@student.pg.edu.pl>
2023-03-03 19:52:26 +0100

    added database_player

---
+++ dnd-bot/dnd_bot/database/database_player.py
@@ -0,0 +1,10 @@
+class DatabasePlayer:
+
+    @staticmethod
+    def add_player(id_user: int = 0, alignment: str = "", backstory: str = "", id_equipment: int = 0,
+                   id_creature: int = 0) -> int | None:
+        pass
+
+    @staticmethod
+    def get_player(id_player) -> dict | None:
+        pass
