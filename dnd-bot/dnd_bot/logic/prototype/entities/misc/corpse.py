5e10b8c76b96ba22ca95364421d7377d66ed1e25
Mixss <michalziemiec@wp.pl>
2023-04-06 20:08:16 +0200

    documented corpse constructor

--- dnd-bot/dnd_bot/logic/prototype/entities/misc/corpse.py
+++ dnd-bot/dnd_bot/logic/prototype/entities/misc/corpse.py
@@ -7,6 +7,7 @@ class Corpse(Entity):
     sprite_path = "dnd_bot/assets/gfx/entities/hole.png"
 
     def __init__(self, x, y, game_token, sprite_path=None):
+        """creates corpse entity with default sprite, but you can pass path to other sprite"""
         if sprite_path is not None:
             Corpse.sprite_path = sprite_path
         super().__init__(x=x, y=y, game_token=game_token, name=Corpse.entity_name, sprite=Corpse.sprite_path, fragile=True)
