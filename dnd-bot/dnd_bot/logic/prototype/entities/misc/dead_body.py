514a1173385069235d495cd07396c3775f5d751f
Mixss <michalziemiec@wp.pl>
2023-04-06 13:24:50 +0200

    enhanced dead body class

--- dnd-bot/dnd_bot/logic/prototype/entities/misc/dead_body.py
+++ dnd-bot/dnd_bot/logic/prototype/entities/misc/dead_body.py
@@ -3,5 +3,10 @@ from dnd_bot.logic.prototype.entity import Entity
 
 class DeadBody(Entity):
 
-    sprite_path = "dnd_bot/assets/gfx/entities/hole.png"
     entity_name = "Dead body"
+    sprite_path = "dnd_bot/assets/gfx/entities/hole.png"
+
+    def __init__(self, x, y, game_token, sprite_path=None):
+        if sprite_path is not None:
+            DeadBody.sprite_path = sprite_path
+        super().__init__(x=x, y=y, game_token=game_token, name=DeadBody.entity_name, sprite=DeadBody.sprite_path)
