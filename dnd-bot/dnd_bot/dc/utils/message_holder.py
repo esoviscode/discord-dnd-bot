6a3e3a984de4063ea8012c025ed8efadebf0a675
Cellmer <michal.cellmer@gmail.com>
2023-04-12 22:52:00 +0200

    dc: utils: message_holder.py: add type hinting

--- dnd-bot/dnd_bot/dc/utils/message_holder.py
+++ dnd-bot/dnd_bot/dc/utils/message_holder.py
@@ -1,9 +1,9 @@
 class MessageHolder:
     """This class holds data of the last message that was sent to each user"""
 
-    # keys of this dict are user discord ids; values are couples of channel id and message_id
-    user_last_message_data = {}
-    user_last_error_message_data = {}
+    # keys of this dict are tuples of user discord ids and game tokens; values are couples of channel id and message_id
+    user_last_message_data: dict[(int, str), (int, int)] = {}
+    user_last_error_message_data: dict[(int, str), (int, int)] = {}
 
     @staticmethod
     def register_last_message_data(user_id: int, channel_id: int, message_id: int):
