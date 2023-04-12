e7485a355f03677aaba92d3c097cdc3c55e050cf
Cellmer <michal.cellmer@gmail.com>
2023-04-12 23:03:55 +0200

    dc: utils: message_holder.py: add game token to each method

--- dnd-bot/dnd_bot/dc/utils/message_holder.py
+++ dnd-bot/dnd_bot/dc/utils/message_holder.py
@@ -6,32 +6,32 @@ class MessageHolder:
     user_last_error_message_data: dict[(int, str), (int, int)] = {}
 
     @staticmethod
-    def register_last_message_data(user_id: int, channel_id: int, message_id: int):
-        """saves last message data for given discord user id"""
-        MessageHolder.user_last_message_data[str(user_id)] = (channel_id, message_id)
+    def register_last_message_data(user_id: int, token: str, channel_id: int, message_id: int):
+        """save last message data for given discord user id and game token"""
+        MessageHolder.user_last_message_data[(user_id, token)] = (channel_id, message_id)
 
     @staticmethod
-    def read_last_message_data(user_id: int):
-        """read last message data for given discord user id"""
-        if not str(user_id) in MessageHolder.user_last_message_data:
+    def read_last_message_data(user_id: int, token: str):
+        """read last message data for given discord user id and game token"""
+        if not (user_id, token) in MessageHolder.user_last_message_data:
             return None
         else:
-            return MessageHolder.user_last_message_data[str(user_id)]
+            return MessageHolder.user_last_message_data[(user_id, token)]
 
     @staticmethod
-    def register_last_error_data(user_id: int, channel_id: int, message_id: int):
-        """saves last message data for given discord user id"""
-        MessageHolder.user_last_error_message_data[str(user_id)] = (channel_id, message_id)
+    def register_last_error_data(user_id: int, token: str, channel_id: int, message_id: int):
+        """save last error data for given discord user id and game token"""
+        MessageHolder.user_last_error_message_data[(user_id, token)] = (channel_id, message_id)
 
     @staticmethod
-    def read_last_error_data(user_id: int):
-        """read last message data for given discord user id"""
-        if not str(user_id) in MessageHolder.user_last_error_message_data:
+    def read_last_error_data(user_id: int, token: str):
+        """read last error data for given discord user id and game token"""
+        if not (user_id, token) in MessageHolder.user_last_error_message_data:
             return None
         else:
-            return MessageHolder.user_last_error_message_data[str(user_id)]
+            return MessageHolder.user_last_error_message_data[(user_id, token)]
 
     @staticmethod
-    def delete_last_error_data(user_id: int):
-        MessageHolder.user_last_error_message_data[str(user_id)] = None
+    def delete_last_error_data(user_id: int, token: str):
+        MessageHolder.user_last_error_message_data[(user_id, token)] = None
 
