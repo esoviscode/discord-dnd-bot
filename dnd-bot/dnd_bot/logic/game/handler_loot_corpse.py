f71bba28442ba8428225b6d05cad7a14ac50c0f9
Mixss <michalziemiec@wp.pl>
2023-04-27 12:04:37 +0200

    fixed removing wrong corpse after looting one

--- dnd-bot/dnd_bot/logic/game/handler_loot_corpse.py
+++ dnd-bot/dnd_bot/logic/game/handler_loot_corpse.py
@@ -19,12 +19,14 @@ class HandlerLootCorpse:
         if not corpse:
             raise Exception("Player tried to loot the corpse when there is not one nearby!")
 
+        print(corpse.id)
+
         money = corpse.dropped_money
         items = corpse.dropped_items
         corpse_name = corpse.creature_name
 
         game: Game = Multiverse.get_game(player.game_token)
-        game.delete_entity(corpse.id)
+        game.delete_entity_at(corpse.x, corpse.y)
 
         player.add_money(money)
         player.add_items(items)
