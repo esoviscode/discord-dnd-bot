385eb7798888c3e719981dc8482404c60e8076a3
Laptop-Acer <s184469@student.pg.edu.pl>
2023-05-09 13:22:37 +0200

    added dialogue placeholder

--- dnd-bot/dnd_bot/logic/game/handler_dialogue.py
+++ dnd-bot/dnd_bot/logic/game/handler_dialogue.py
@@ -1,10 +1,7 @@
-from dnd_bot.dc.utils.handler_views import HandlerViews
-
-
 class HandlerDialogue:
 
     @staticmethod
-    async def handle_talk(player):
+    async def handle_talk(player, target):
         """ handles giving loot for the player and removing the corpse from the world """
 
-        await HandlerViews.display_views_for_users(player.game_token, "")
+        return f"{target.name}: \"Hello, {player.name}!\""
