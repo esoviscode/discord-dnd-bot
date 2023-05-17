51e47805d43e567cc82f20b8f1a062183db8f3b8
Cellmer <michal.cellmer@gmail.com>
2023-05-17 13:00:56 +0200

    database: database_creature.py: add max_hp and initial_action points attributes to creature related database methods

--- dnd-bot/dnd_bot/database/database_creature.py
+++ dnd-bot/dnd_bot/database/database_creature.py
@@ -9,19 +9,22 @@ class DatabaseCreature:
                      dexterity: int = 0, intelligence: int = 0, charisma: int = 0, perception: int = 0,
                      initiative: int = 0, action_points: int = 0, level: int = 0, money: int = 0,
                      id_game: int = 1, experience: int = 0, id_equipment: int = None, creature_class: str = None,
-                     description: str = '') -> int | None:
+                     description: str = '', max_hp: int = 0, initial_action_points: int = 0) -> int | None:
         id_entity = DatabaseEntity.add_entity(name=name, x=x, y=y, id_game=id_game, description=description)
         if creature_class is not None:
             creature_class = creature_class.upper()
 
         id_creature = DatabaseConnection.add_to_db('INSERT INTO public."Creature" (level, "HP", strength, dexterity, '
                                                    'intelligence, charisma, perception, initiative, action_points, '
-                                                   'money, id_entity, experience, id_equipment, class) '
-                                                   'VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)',
+                                                   'money, id_entity, experience, id_equipment, class, max_hp, '
+                                                   'initial_action_points)'
+                                                   'VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, '
+                                                   '%s, %s)',
                                                    (
                                                        level, hp, strength, dexterity, intelligence,
                                                        charisma, perception, initiative, action_points,
-                                                       money, id_entity, experience, id_equipment, creature_class),
+                                                       money, id_entity, experience, id_equipment, creature_class,
+                                                       max_hp, initial_action_points),
                                                    "creature")
         return id_creature
 
@@ -30,14 +33,15 @@ class DatabaseCreature:
                            dexterity: int = 0, intelligence: int = 0, charisma: int = 0, perception: int = 0,
                            initiative: int = 0, action_points: int = 0, level: int = 0, money: int = 0,
                            id_game: int = 1, experience: int = 0, id_equipment: int = None, creature_class: str = None,
-                           description: str = '', id_entity: int = 0) -> tuple[str, tuple]:
+                           description: str = '', id_entity: int = 0, max_hp: int = 0,
+                           initial_action_points: int = 0) -> tuple[str, tuple]:
         return 'INSERT INTO public."Creature" (level, "HP", strength, dexterity, ' \
                'intelligence, charisma, perception, initiative, action_points, ' \
                'money, id_entity, experience, id_equipment, class) ' \
-               'VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)', \
+               'VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)', \
             (level, hp, strength, dexterity, intelligence,
              charisma, perception, initiative, action_points,
-             money, id_entity, experience, id_equipment, creature_class)
+             money, id_entity, experience, id_equipment, creature_class, max_hp, initial_action_points)
 
     @staticmethod
     def update_creature(id_creature: int = 0, hp: int = 0, level: int = 0, money: int = 0, experience: int = 0,
@@ -56,7 +60,7 @@ class DatabaseCreature:
         creature = {'id_creature': db_t[0], 'level': db_t[1], 'hp': db_t[2], 'strength': db_t[3], 'dexterity': db_t[4],
                     'intelligence': db_t[5], 'charisma': db_t[6], 'perception': db_t[7], 'initiative': db_t[8],
                     'action_points': db_t[9], 'money': db_t[10], 'id_entity': db_t[11], 'experience': db_t[12],
-                    'id_equipment': db_t[13], 'class': db_t[14]}
+                    'id_equipment': db_t[13], 'class': db_t[14], 'max_hp': db_t[15], 'initial_action_points': db_t[16]}
         entity = DatabaseEntity.get_entity(creature['id_entity'])
         for key, value in entity.items():
             creature[key] = value
